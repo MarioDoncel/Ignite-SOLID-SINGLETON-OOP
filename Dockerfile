26869df2ea70381f0c4c9e19ec1758ef1cd13709
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2022-01-25 22:34:41 -0300

    Feat: docker + docker compose + typeorm + pg

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node
+
+WORKDIR /usr/app
+COPY package.json /usr/app/
+
+RUN npm install 
+
+COPY . /usr/app/
+
+EXPOSE 3333
+
+CMD ["npm", "run", "dev"]
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express'
-import categoriesRoutes  from './categories.routes'
-import specificationRoutes  from './specification.routes'
+import categoriesRoutes from './categories.routes'
+import specificationRoutes from './specification.routes'
 
 const router = express.Router()
 
@@ -8,8 +8,8 @@ router.use('/categories', categoriesRoutes)
 router.use('/specification', specificationRoutes)
 
 
-router.get('/', (req:Request, res:Response, next:NextFunction)=>{
-    return res.status(200).send(`Hello Mario!`)
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  return res.status(200).send(`Hello Mario!`)
 })
 
 
